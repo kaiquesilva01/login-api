76fb1c853e2a41603184183c6667e903a0144d05
Kaique Silva <kaique.andruid@gmail.com>
2024-05-20 14:47:47 -0300

    Manual Commit  "Start Project & User implement"

---
+++ backend/src/main/java/com/systemweb/loginapi/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.systemweb.loginapi.resources;
+
+import com.systemweb.loginapi.entities.User;
+import com.systemweb.loginapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+}
---
+++ backend/src/test/java/com/systemweb/loginapi/LoginapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemweb.loginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
