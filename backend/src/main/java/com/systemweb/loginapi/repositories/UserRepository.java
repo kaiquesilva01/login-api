1270d62e9c30133bbbc687e9c345b44a84a14fb5
Kaique Silva <kaique.andruid@gmail.com>
2024-05-22 06:32:19 -0300

    Settings

--- backend/src/main/java/com/systemweb/loginapi/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.systemweb.loginapi.repositories;
-
-import com.systemweb.loginapi.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
