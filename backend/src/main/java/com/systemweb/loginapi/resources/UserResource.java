06e36fa08c5865fe2ec5c55bb0cab5bfe86004e4
Kaique <kaique.andruid@gmail.com>
2024-05-22 23:00:19 -0300

    Error "Access-Control-Allow-Origin" Resolved

--- backend/src/main/java/com/systemweb/loginapi/resources/UserResource.java
+++ backend/src/main/java/com/systemweb/loginapi/resources/UserResource.java
@@ -5,10 +5,7 @@ import com.systemweb.loginapi.entities.User;
 import com.systemweb.loginapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +16,8 @@ public class UserResource {
 
     @Autowired
     private UserService service;
+
+    @CrossOrigin(origins = "*")
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
         List<UserDTO> list = service.findAll();
